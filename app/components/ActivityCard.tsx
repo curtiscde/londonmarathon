925f1b790cd3df4164b4fdf1364b63972620b2fb
Curtis Timson <curt@live.co.uk>
2024-09-28 14:35:37 +0100

    UI Layout (#4)

---
+++ app/components/ActivityCard.tsx
@@ -0,0 +1,13 @@
+export default function ActivityCard() {
+  return (
+    <div className="card bg-base-300 rounded-box place-items-center">
+      <div className="card-body items-center">
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus mauris volutpat, elementum libero nec, venenatis metus. Curabitur ullamcorper purus ut libero sollicitudin, a blandit odio tincidunt. Curabitur porta magna est, sed luctus magna pellentesque ac. Sed in nisi magna. Sed dapibus, sem pellentesque finibus sollicitudin, metus nulla sodales justo, sed gravida quam dolor quis justo. Integer vel porta lacus. Integer et sollicitudin velit.
+        </p>
+
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import ActivityCard from "./components/ActivityCard";
+import DonationCard from "./components/DonationCard";
 import Header from "./components/Header"
 
 export default function Home() {
@@ -5,8 +7,127 @@ export default function Home() {
     <div>
       <Header />
       <main>
+        <div className="container mx-auto">
+          <div className="grid grid-cols-12 gap-4">
+            <div className="grid col-span-9">
+              <div className="card bg-base-300 rounded-box place-items-center">
+                <div className="card-body items-center text-center">
+                  <img src="/image.png" />
+                </div>
+              </div>
+            </div>
+            <div className="grid col-span-3">
+              <div className="card bg-base-300 rounded-box place-items-center">
+                <div className="card-body items-center text-center">
 
-      </main>
+
+                  <div
+                    className="radial-progress bg-primary text-primary-content border-primary border-4"
+                    style={{ "--value": 70 }}
+                    role="progressbar">
+                    70%
+                  </div>
+
+                </div>
+              </div>
+            </div>
+            <div className="grid col-span-12">
+              <div className="card bg-base-300 rounded-box place-items-center">
+                <div className="card-body items-center">
+
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus mauris volutpat, elementum libero nec, venenatis metus. Curabitur ullamcorper purus ut libero sollicitudin, a blandit odio tincidunt. Curabitur porta magna est, sed luctus magna pellentesque ac. Sed in nisi magna. Sed dapibus, sem pellentesque finibus sollicitudin, metus nulla sodales justo, sed gravida quam dolor quis justo. Integer vel porta lacus. Integer et sollicitudin velit.
+                  </p>
+
+                </div>
+              </div>
+            </div>
+            <div className="grid col-span-12">
+              <div className="divider">Recent Donations</div>
+            </div>
+
+            <div className="grid col-span-4">
+              <DonationCard />
+            </div>
+            <div className="grid col-span-4">
+              <DonationCard />
+            </div>
+            <div className="grid col-span-4">
+              <DonationCard />
+            </div>
+            <div className="grid col-span-4">
+              <DonationCard />
+            </div>
+            <div className="grid col-span-4">
+              <DonationCard />
+            </div>
+            <div className="grid col-span-4">
+              <DonationCard />
+            </div>
+
+            <div className="grid col-span-12 place-items-center">
+              <button className="btn btn-wide">Load More Donations</button>
+            </div>
+
+            <div className="grid col-span-12">
+              <div className="divider">Recent Activities</div>
+            </div>
+
+            <div className="grid col-span-4">
+              <ActivityCard />
+            </div>
+            <div className="grid col-span-4">
+              <ActivityCard />
+            </div>
+            <div className="grid col-span-4">
+              <ActivityCard />
+            </div>
+            <div className="grid col-span-4">
+              <ActivityCard />
+            </div>
+            <div className="grid col-span-4">
+              <ActivityCard />
+            </div>
+            <div className="grid col-span-4">
+              <ActivityCard />
+            </div>
+
+            <div className="grid col-span-12 place-items-center">
+              <button className="btn btn-wide">Load More Activities</button>
+            </div>
+
+            <div className="grid col-span-12">
+              <div className="divider"></div>
+            </div>
+
+            <div className="grid col-span-12">
+              <footer className="footer bg-neutral text-neutral-content p-10">
+                <nav>
+                  <h6 className="footer-title">Foo</h6>
+                  <a className="link link-hover" target="_blank" href="https://www.tcslondonmarathon.com">TCS London Marathon</a>
+                  <a className="link link-hover">Foo</a>
+                  <a className="link link-hover">Bar</a>
+                  <a className="link link-hover">Baz</a>
+                </nav>
+                <nav>
+                  <h6 className="footer-title">Bar</h6>
+                  <a className="link link-hover" target="_blank" href="https://www.tcslondonmarathon.com">TCS London Marathon</a>
+                  <a className="link link-hover">Foo</a>
+                  <a className="link link-hover">Bar</a>
+                  <a className="link link-hover">Baz</a>
+                </nav>
+                <nav>
+                  <h6 className="footer-title">Baz</h6>
+                  <a className="link link-hover" target="_blank" href="https://www.tcslondonmarathon.com">TCS London Marathon</a>
+                  <a className="link link-hover">Foo</a>
+                  <a className="link link-hover">Bar</a>
+                </nav>
+              </footer>
+            </div>
+
+          </div>
+        </div>
+      </main >
       <footer>
 
       </footer>
