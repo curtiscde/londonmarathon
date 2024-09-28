9992c82230ab05cefdb00a00834f9b5fab59e313
curtiscde <curt@live.co.uk>
2024-09-28 12:37:24 +0100

    Header

---
+++ app/components/Header.tsx
@@ -0,0 +1,30 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16" />
+              </svg>
+            </div>
+          </div>
+          <a className="btn btn-ghost text-xl">London Marathon 2025</a>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Donate Now</a>
+        </div>
+      </div>
+    </header>
+  );
+}
