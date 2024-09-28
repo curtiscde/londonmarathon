c8e2e910b65fff5049d687acbe1d2f21f2a4eaf6
curtiscde <curt@live.co.uk>
2024-09-28 13:58:11 +0100

    Map UI

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,31 @@ export default function Home() {
     <div>
       <Header />
       <main>
+        <div className="container mx-auto">
+          <div className="grid grid-cols-12 gap-4">
+            <div className="grid col-span-9">
+              <div className="card bg-base-300 rounded-box place-items-center">
+                <div className="card-body items-center text-center">
+                  <img src="/image.png" />
+                </div>
+              </div>
+            </div>
+            <div className="grid col-span-3">
+              <div className="card bg-base-300 rounded-box place-items-center">
+                <div className="card-body items-center text-center">
 
-      </main>
+
+
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main >
       <footer>
 
       </footer>
-    </div>
+    </div >
   );
 }
