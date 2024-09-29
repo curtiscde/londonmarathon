5248c07299e5f0740b10716680f7fa747ec09832
curtiscde <curt@live.co.uk>
2024-09-29 20:15:55 +0100

    Strava: Recent Activities

--- app/components/ActivityCard.tsx
+++ app/components/ActivityCard.tsx
@@ -1,8 +1,14 @@
-export default function ActivityCard() {
+import { Activity } from "@/types/Activity";
+
+interface ActivityCardProps {
+  activity: Activity
+}
+
+export default function ActivityCard({ activity }: ActivityCardProps) {
   return (
     <div className="card bg-base-300 rounded-box place-items-center">
       <div className="card-body items-center">
-
+        <h2 className="card-title">{activity.name}</h2>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus mauris volutpat, elementum libero nec, venenatis metus. Curabitur ullamcorper purus ut libero sollicitudin, a blandit odio tincidunt. Curabitur porta magna est, sed luctus magna pellentesque ac. Sed in nisi magna. Sed dapibus, sem pellentesque finibus sollicitudin, metus nulla sodales justo, sed gravida quam dolor quis justo. Integer vel porta lacus. Integer et sollicitudin velit.
         </p>
