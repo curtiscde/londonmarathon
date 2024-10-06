4aacbcefbbad1a78ecc7e053b87714fd4a6062ff
curtiscde <curt@live.co.uk>
2024-10-06 20:45:19 +0100

    Donation UI

--- app/justGiving/types/JustGivingDonation.ts
+++ app/justGiving/types/JustGivingDonation.ts
@@ -1,8 +1,17 @@
 import { z } from "zod";
 
-export const justGivingDonationSchema = z.object({
-  id: z.number(),
-  donorDisplayName: z.string(),
-})
+export const justGivingDonationSchema = z
+  .object({
+    id: z.number(),
+    donorDisplayName: z.string(),
+    donationDate: z.string(),
+  })
+  .transform((donation) => {
+    return {
+      id: donation.id,
+      donorDisplayName: donation.donorDisplayName,
+      donationDate: new Date(Number(donation.donationDate.match(/Date\((\d+)\+\d+\)/)![1])).toUTCString()
+    }
+  })
 
 export type JustGivingDonation = z.infer<typeof justGivingDonationSchema>
