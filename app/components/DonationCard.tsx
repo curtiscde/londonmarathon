3a33c3aa45546ad108c6b550f831dabea2d948b9
curtiscde <curt@live.co.uk>
2024-10-06 21:04:15 +0100

    display message and amount

--- app/components/DonationCard.tsx
+++ app/components/DonationCard.tsx
@@ -19,10 +19,17 @@ export default function DonationCard({ donation }: DonationCardProps) {
             sameElse: "DD/MM/YYYY [at] h:mm A"
           })}
         </h6>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla rhoncus mauris volutpat, elementum libero nec, venenatis metus. Curabitur ullamcorper purus ut libero sollicitudin, a blandit odio tincidunt. Curabitur porta magna est, sed luctus magna pellentesque ac. Sed in nisi magna. Sed dapibus, sem pellentesque finibus sollicitudin, metus nulla sodales justo, sed gravida quam dolor quis justo. Integer vel porta lacus. Integer et sollicitudin velit.
-        </p>
-
+        <div className="stats shadow w-full mt-2 mb-4">
+          <div className="stat">
+            <div className="stat-title">Donation</div>
+            <div className="stat-value">&pound;{donation.amount}</div>
+          </div>
+        </div>
+        <blockquote>
+          <p>
+            &quot;{donation.message}&quot;
+          </p>
+        </blockquote>
       </div>
     </div>
   );
--- app/justGiving/types/JustGivingDonation.ts
+++ app/justGiving/types/JustGivingDonation.ts
@@ -2,15 +2,19 @@ import { z } from "zod";
 
 export const justGivingDonationSchema = z
   .object({
+    amount: z.string(),
     id: z.number(),
     donorDisplayName: z.string(),
     donationDate: z.string(),
+    message: z.string(),
   })
   .transform((donation) => {
     return {
+      amount: donation.amount,
       id: donation.id,
       donorDisplayName: donation.donorDisplayName,
-      donationDate: new Date(Number(donation.donationDate.match(/Date\((\d+)\+\d+\)/)![1])).toUTCString()
+      donationDate: new Date(Number(donation.donationDate.match(/Date\((\d+)\+\d+\)/)![1])).toUTCString(),
+      message: donation.message
     }
   })
 
