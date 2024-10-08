91f33f02460a03060cc0747f0ebac05332a3e3be
curtiscde <curt@live.co.uk>
2024-10-08 20:16:51 +0100

    update map positioning

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -7,10 +7,12 @@ import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility
 import "./map.css"
 import { coords } from "./lmCoords.json"
 
+const mapCenter = { lat: 51.4955006, lng: -0.0408809 }
+
 export default function Map() {
   return (
     <div className="flex justify-center w-full overflow-hidden">
-      <MapContainer center={{ lat: 51.5072, lng: 0.1276 }} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={mapCenter} zoom={12} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -20,7 +22,7 @@ export default function Map() {
             Test Popup
           </Popup>
         </Marker>
-        <Polyline pathOptions={{ color: 'yellow' }} positions={coords.map(({ lat, lon }) => [Number(lat), Number(lon)])} />
+        <Polyline pathOptions={{ color: 'grey', weight: 4 }} positions={coords.map(({ lat, lon }) => [Number(lat), Number(lon)])} />
       </MapContainer>
     </div>
   )
