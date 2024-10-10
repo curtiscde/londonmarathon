4be7ff1372e2244e3833657eeb157b2582106326
curtiscde <curt@live.co.uk>
2024-10-10 18:14:26 +0100

    Add unit testing

---
+++ app/justGiving/getRecentDonations.test.ts
@@ -0,0 +1,21 @@
+import { getRecentDonations } from "./getRecentDonations"
+import { JustGivingDonation } from "./types/JustGivingDonation";
+
+describe('getRecentDonations.ts', () => {
+  let donations: JustGivingDonation[]
+
+  beforeAll(async () => {
+    donations = await getRecentDonations();
+  })
+
+  it('returns donations', () => {
+    expect(donations).toEqual([
+      { "amount": "2.00", "donationDate": "Fri, 04 Oct 2024 17:37:36 GMT", "donorDisplayName": "display-name", "id": 1, "message": "message" },
+      { "amount": "2.00", "donationDate": "Fri, 04 Oct 2024 17:37:36 GMT", "donorDisplayName": "display-name", "id": 1, "message": "message" },
+      { "amount": "2.00", "donationDate": "Fri, 04 Oct 2024 17:37:36 GMT", "donorDisplayName": "display-name", "id": 1, "message": "message" },
+      { "amount": "2.00", "donationDate": "Fri, 04 Oct 2024 17:37:36 GMT", "donorDisplayName": "display-name", "id": 1, "message": "message" },
+      { "amount": "2.00", "donationDate": "Fri, 04 Oct 2024 17:37:36 GMT", "donorDisplayName": "display-name", "id": 1, "message": "message" },
+      { "amount": "2.00", "donationDate": "Fri, 04 Oct 2024 17:37:36 GMT", "donorDisplayName": "display-name", "id": 1, "message": "message" }
+    ])
+  })
+})
