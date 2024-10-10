55f64c4bcd6e2834bdff110f6dadb400d06f3dae
Curtis Timson <curt@live.co.uk>
2024-10-10 17:39:18 +0100

    MapTiler Setup (#15)

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MapContainer, Marker, Polyline, Popup, TileLayer } from "react-leaflet"
+import { MapContainer, Polyline, Popup, TileLayer } from "react-leaflet"
 import "leaflet/dist/leaflet.css"
 import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
@@ -8,21 +8,21 @@ import "./map.css"
 import { coords } from "./lmCoords.json"
 
 const mapCenter = { lat: 51.4955006, lng: -0.0408809 }
+const mapTilerKey = process.env.NEXT_PUBLIC_MAPTILER_KEY
 
 export default function Map() {
   return (
     <div className="flex justify-center w-full overflow-hidden">
       <MapContainer center={mapCenter} zoom={12} scrollWheelZoom={false}>
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://maptiler.com/">MapTiler</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url={`https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${mapTilerKey}`}
         />
-        <Marker position={{ lat: 51.5072, lng: 0.1276 }}>
+        <Polyline pathOptions={{ color: 'grey', weight: 4 }} positions={coords.map(({ lat, lon }) => [Number(lat), Number(lon)])}>
           <Popup>
             Test Popup
           </Popup>
-        </Marker>
-        <Polyline pathOptions={{ color: 'grey', weight: 4 }} positions={coords.map(({ lat, lon }) => [Number(lat), Number(lon)])} />
+        </Polyline>
       </MapContainer>
     </div>
   )
